2b4541761143e6e930f7c93051000f6ab857ceb8
Fabio Tovar <fabiostovarr@gmail.com>
2020-10-04 20:45:24 -0500

    Convolution finished

--- src/components/articles/ConvolutionImage.js
+++ src/components/articles/ConvolutionImage.js
@@ -1,59 +1,13 @@
-import React, {  useState } from 'react'
+import React, { useState } from 'react'
 import { loadableP5 as P5Wrapper } from '../../sketches/lodable'
-import Slider from 'react-rangeslider'
-import convolution from '../../sketches/convolution'
+import image from '../../sketches/convolution/image'
 
-const ConvolutionImage = () => {
-  const { kernels, image } = convolution
-
-  const [tmp, setTmp] = useState(3)
-  const [kernelSize, setKernelSize] = useState(3)
-  const [kernel, setKernel] = useState(0)
-
-  const handleClick = () => {
-    setKernel((kernel + 1) % kernels.length)
-    setTmp(3)
-    setKernelSize(3)
-  }
-
-  const onChangeComplete = () => {
-    setKernelSize(tmp)
-  }
-
+const ConvolutionImage = ({ kernel, kernelSize }) => {
   return (
     <>
-      <h1>{kernels[kernel].name}</h1>
-
-      <div style={{ textAlign: 'center' }} onClick={handleClick}>
-        <P5Wrapper
-          sketch={image}
-          kernel={kernels[kernel]}
-          kernelSize={kernelSize}
-        />
+      <div style={{ textAlign: 'center' }}>
+        <P5Wrapper sketch={image} kernel={kernel} kernelSize={kernelSize} />
       </div>
-
-      {kernels[kernel].id === 'gaussian' && (
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            marginTop: 15,
-          }}
-        >
-          <div style={{ width: 400 }}>
-            <h5>Kernel size: </h5>
-            <Slider
-              min={3}
-              max={17}
-              step={2}
-              width={250}
-              value={tmp}
-              onChangeComplete={onChangeComplete}
-              onChange={setTmp}
-            />
-          </div>
-        </div>
-      )}
     </>
   )
 }
--- src/sketches/convolution/shader.js
+++ src/sketches/convolution/shader.js
@@ -3,7 +3,12 @@ import convolutionVert from 'file-loader!./convolution.vert'
 import bomba from 'file-loader!../../videos/bomba.mov'
 
 export default p5 => {
-  let shader, fingers, canvas
+  let shader,
+    fingers,
+    canvas,
+    button,
+    playing = true,
+    kernel
 
   p5.preload = () => {
     shader = p5.loadShader(convolutionVert, convolutionFrag)
@@ -13,17 +18,37 @@ export default p5 => {
     })
   }
 
+  p5.myCustomRedrawAccordingToNewPropsHandler = function(props) {
+    if (props.kernel) {
+      kernel = props.kernel.flat()
+      console.log(kernel)
+      fingers.time(0)
+      fingers.loop()
+    }
+  }
+
   p5.setup = () => {
     p5.noStroke()
   }
 
   p5.draw = () => {
-    if (canvas) {
+    if (canvas && kernel) {
       p5.shader(shader)
-      shader.setUniform('kernel', [0, -1, 0, -1, 5, -1, 0, -1, 0])
+      shader.setUniform('kernel', kernel)
       shader.setUniform('textureSize', [p5.width, p5.height])
       shader.setUniform('tex0', fingers)
       p5.rect(0, 0, p5.width, p5.height)
     }
+  }
+
+  function toggleVid() {
+    if (playing) {
+      fingers.pause()
+      button.html('play')
+    } else {
+      fingers.loop()
+      button.html('pause')
+    }
+    playing = !playing
   }
 }
