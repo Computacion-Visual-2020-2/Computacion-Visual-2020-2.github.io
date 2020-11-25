7ca9ef5287a2c047167dd029171be14ce069760e
Fabio Tovar <fabiostovarr@gmail.com>
2020-11-24 23:58:22 -0500

    Removed useless component

--- src/components/LightShaders/AllLights.js
+++ src/components/LightShaders/AllLights.js
@@ -18,46 +18,6 @@ export default () => {
           lightColor={lightColor}
         />
       </div>
-      {/* <div>
-        <h5>Intensity: </h5>
-        <Slider
-          min={0}
-          max={100}
-          step={2}
-          width={250}
-          value={intensity}
-          onChange={setIntensity}
-        />
-      </div> */}
-      <div style={{ marginTop: 40 }}>
-        <h5>Color: </h5>
-        <CirclePicker
-          width="100%"
-          color={lightColor}
-          onChangeComplete={color => setLightColor(color.rgb)}
-          colors={[
-            '#fff',
-            '#f44336',
-            '#9c27b0',
-            '#673ab7',
-            '#3f51b5',
-            '#2196f3',
-            '#03a9f4',
-            '#00bcd4',
-            '#009688',
-            '#4caf50',
-            '#8bc34a',
-            '#cddc39',
-            '#ffeb3b',
-            '#ffc107',
-            '#ff9800',
-            '#ff5722',
-            '#795548',
-            '#607d8b',
-          ]}
-          circleSize={50}
-        />
-      </div>
     </>
   )
 }
