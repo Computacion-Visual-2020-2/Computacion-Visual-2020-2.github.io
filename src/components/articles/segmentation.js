20916baf5c4732a125d425c4776103b9471b821c
Fabio Tovar <fabiostovarr@gmail.com>
2020-12-10 09:07:05 -0500

    Segmentation section finished

--- src/components/articles/segmentation.js
+++ src/components/articles/segmentation.js
@@ -2,5 +2,5 @@ import React from 'react'
 import Discussion from '../Segmentation/Discussion'
 
 export default [
-  { id: 'discussion', title: 'Discusión', body: () => <Discussion /> },
+  { id: 'discussion', title: 'Histograma por clústeres', body: () => <Discussion /> },
 ]
